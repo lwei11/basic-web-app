c1dd553e4a768c85f2517cb5d4d2a77eee06d0dd
lws-Macbook-Pro <lanyuntw@andrew.cmu.edu>
2023-09-19 16:08:59 -0400

    largest

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -62,5 +62,28 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("largest")) {
+    var res = query.match(/d+/g)
+
+    // Use regular expressions to find integers in the string
+    const integerRegex = /\d+/g; // Matches one or more digits
+
+    const integersFound = query.match(integerRegex);
+
+    var result = 0
+    if (integersFound && integersFound.length >= 3) {
+      const firstInteger = parseInt(integersFound[0]);
+      const secondInteger = parseInt(integersFound[1]);
+      const thirdInteger = parseInt(integersFound[2]);
+      // Perform addition
+      result = Math.max(firstInteger, secondInteger, thirdInteger);
+    }
+    
+    return (
+      result.toString()
+    );
+  }
+
+
   return "";
 }
